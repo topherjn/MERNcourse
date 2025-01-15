e3e369d7c39b6b888a774b1478dc89392b613c46
topherjn <nicholsonc@gmail.com>
2025-01-15 16:52:43 -0500

    Home and About elements added Step 3.17

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
 import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  {
+    path:'/',
+    element:<h1>home</h1>,
+  },
+{
+  path:'/about',
+  element:<div><h1>about page</h1></div>,
+}
+)
 
 const App = () => {
   return (
