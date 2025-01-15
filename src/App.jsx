e8c55e389feae8b27e9fd531eb452265d50d2bde
topherjn <nicholsonc@gmail.com>
2025-01-15 17:14:13 -0500

    Had to just paste App.jsx from README in the end

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter(
-[{
-    path:'/',
-    element:<h1>home</h1>,
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>home</h1>,
   },
-{
-  path:'/about',
-  element:<div><h1>about page</h1></div>,
-}
-])
+  {
+    path: '/about',
+    element: (
+      <div>
+        <h2>about page</h2>
+      </div>
+    ),
+  },
+]);
 
 const App = () => {
-  return (
-    <h1>
-     Jobify App
-    </h1>
-  )
-}
-
-export default App
+  return <RouterProvider router={router} />;
+};
+export default App;
