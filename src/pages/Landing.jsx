541c25d4f9d1e60a382dbc56cf54ade698ddbd18
topherjn <nicholsonc@gmail.com>
2025-01-16 13:32:17 -0500

    Styled component button and syntax highlighting extension for styled components Step 4.23

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import styled from 'styled-components'
+
+const StyledBtn = styled.button`
+  font-size: 1.5rem;
+  color:white;
+`;
 
 const Landing = () => {
-  return (
-    <h1>
-      Landing Page
-    </h1>
-  )
+  return <div><h1>Landing Page</h1>
+  <StyledBtn>Button</StyledBtn> </div>
 }
 
 export default Landing
