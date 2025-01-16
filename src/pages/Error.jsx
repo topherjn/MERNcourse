ff6da51fccf15b945b778eb3d1e0b425131004f6
topherjn <nicholsonc@gmail.com>
2025-01-16 16:16:20 -0500

    Error page created Step 5.29

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
 import { Link, useRouteError } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/images/not-found.svg';
 
 const Error = () => {
-    const error = useRouteError();
-    console.log(error);
+  const error = useRouteError();
+
+  if (error.status == 404) {
+    return(<Wrapper>
+      <div>
+        <img src={img} alt='not found' className='img' />
+        <h3>Page not found</h3>
+        <p>Can't find it</p>
+        <Link to='/dashboard' className='btn'>back to dashboard</Link>
+      </div>
+    </Wrapper>);
+  }
+
+  console.log(error);
   return (
-<div><h1>Error Page</h1>
-<Link to='/'>back home</Link></div>
+  
+    <div><h1>Something Went Wrong</h1>
+      <Link to='/'>back home</Link></div>
   );
 };
 
