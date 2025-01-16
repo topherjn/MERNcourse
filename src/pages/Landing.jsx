5979798ed38c2ea36105ef8671b4df7a9a04051c
topherjn <nicholsonc@gmail.com>
2025-01-16 15:47:11 -0500

    Logo turned into a component Step 4.27

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,12 +2,13 @@ import Wrapper from '../assets/wrappers/LandingPage';
 import main from '../assets/images/main.svg';
 import logo from '../assets/images/logo.svg';
 import { Link } from 'react-router-dom';
+import { Logo } from '../components';
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-       <img src={logo} alt='job hunt' className='logo' />
+       <Logo />
       </nav>
       <div className='container page'>
         <div className='info'>
