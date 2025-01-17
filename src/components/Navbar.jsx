c21c3457b3037f0375700eee3e30980c0cb0f5db
topherjn <nicholsonc@gmail.com>
2025-01-17 18:11:21 -0500

    Toggle sidebar context added Step 8.40

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/Navbar';
-import {FaHome, FaUser, FaCog} from 'react-icons/fa';
+import Logo from './Logo';
+import { FaHome, FaUser, FaCog, FaAlignLeft } from 'react-icons/fa';
+import { useDashboardContext } from '../pages/DashboardLayout';
 
 const Navbar = () => {
+  const {toggleSidebar} = useDashboardContext();
+
   return (
     <Wrapper>
-      <h2>navbar</h2>
-      <FaHome />
+      <div className='nav-center'>
+        <button type='button' className='toggle-btn'>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className='logo-text'>Dashboard</h4>
+        </div>
+        <div className='btn-container'>
+          toggle/logout
+        </div>
+      </div>
     </Wrapper>
   )
 }
