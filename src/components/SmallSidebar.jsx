0ecd8be653a0006c2c8cf09d2b6988ae7cd97341
topherjn <nicholsonc@gmail.com>
2025-01-17 18:40:02 -0500

    Navlinks and icons done Step 8.43

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/SmallSidebar';
 import { useDashboardContext } from '../pages/DashboardLayout';
+import { FaTimes } from 'react-icons/fa';
+import Logo from './Logo';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
 
 const SmallSidebar = () => {
   const data = useDashboardContext();
   console.log(data);
-  
+
   return (
 
     <Wrapper>
-      SmallSidebar
+      <div className='sidebar-container show-sidebar'>
+        <div className='content'>
+          <button type='button' className='close-btn'>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>
+            {links.map((link) => {
+              const { text, path, icon } = link;
+              return (<NavLink to={path} key={text} className='nav-link'>
+                <span className='icon'>{icon}</span> {text}
+              </NavLink>);
+            })}
+          </div>
+        </div></div>
     </Wrapper>
   )
 }
