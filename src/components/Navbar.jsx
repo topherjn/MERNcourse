a5f31780333e3bfc2aee71fd289f752af12ed87d
topherjn <nicholsonc@gmail.com>
2025-01-17 18:01:21 -0500

    Added FaHome icon Step 8.39

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/Navbar';
+import {FaHome, FaUser, FaCog} from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <Wrapper>
-      Navbar
+      <h2>navbar</h2>
+      <FaHome />
     </Wrapper>
   )
 }
