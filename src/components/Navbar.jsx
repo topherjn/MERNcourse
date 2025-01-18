d8b190a58647e0b148871df161ea7ed6e20c5fcb
topherjn <nicholsonc@gmail.com>
2025-01-17 22:39:38 -0500

    Logout button set up Step 8.49

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import Wrapper from '../assets/wrappers/Navbar';
 import { FaAlignLeft } from 'react-icons/fa';
 import Logo from './Logo';
 import { useDashboardContext } from '../pages/DashboardLayout';
+import LogoutContainer from './LogoutContainer';
 
 const Navbar = () => {
   const { toggleSidebar } = useDashboardContext();
@@ -16,7 +17,7 @@ const Navbar = () => {
           <h4 className='logo-text'>dashboard</h4>
         </div>
         <div className='btn-container'>
-
+          <LogoutContainer />
         </div>
       </div>
     </Wrapper>
