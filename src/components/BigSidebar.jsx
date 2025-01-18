64c7f4e661c810f2d5c5cce1dcdfa8680e9b3e1e
topherjn <nicholsonc@gmail.com>
2025-01-17 22:20:51 -0500

    Big SideBar navlinks Step 8.47

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
 import Wrapper from '../assets/wrappers/BigSidebar';
+import { useDashboardContext } from '../pages/DashboardLayout';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 const BigSidebar = () => {
+  const { showSidebar } = useDashboardContext();
+
   return (
     <Wrapper>
-      BigSidebar
+      <div className={showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'}>
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks isBigSidebar/>
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
 export default BigSidebar
