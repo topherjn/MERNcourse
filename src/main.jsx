30ea969da0e7a0091cf593b3a723ec2cc261d7d4
topherjn <nicholsonc@gmail.com>
2025-01-20 19:49:53 -0500

    Client changes added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import customFetch from './utils/customFetch.js';
+
+const data = await customFetch.get('/test');
+console.log(data);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
